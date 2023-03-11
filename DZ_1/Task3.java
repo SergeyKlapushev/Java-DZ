e9971a3098f2f8dadee00b87b02720d9f79d6e43
Sergei Klapushev <klapischeff@yandex.ru>
2023-03-11 13:05:50 +0300

    Task3 added

---
+++ DZ_1/Task3.java
@@ -0,0 +1,19 @@
+package DZ_1;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        
+        int inputNums = imputNumbers();
+        System.out.println(inputNums);
+
+    }
+
+    public static Integer imputNumbers()
+    {
+        Scanner scan = new Scanner(System.in);
+        Integer num = scan;
+        return num;
+    } 
+}
