bd44161f3b40107475d187edfbcdb27a8e703a30
Sergei Klapushev <klapischeff@yandex.ru>
2023-03-11 12:29:57 +0300

    Task1 complited

--- DZ_1/Task1.java
+++ DZ_1/Task1.java
@@ -1,8 +1,29 @@
 package DZ_1;
 
+import java.util.Scanner;
+
+import Scanner;
+/* Вычислить [n-ое треугольного число](сумма чисел от 1 до n), n! (произведение чисел от 1 до n) */
 public class Task1 {
     public static void main(String[] args) {
-    {
         
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите первое число: ");
+        Integer num = iScanner.nextInt();
+        System.out.println(num);
+        int triangleNum = findTriagleNum(num);
+        System.out.println("Треугольное число: " + triangleNum);
+        iScanner.close();
     }
+
+    public static int findTriagleNum(int num) {
+        int triangleNum = 0;
+
+        for (int i = 0; i < num; i++)
+        {
+            triangleNum = (triangleNum + i);
+        }
+        return triangleNum + num;
+    }
+    
 }
--- DZ_1/Task2.java
+++ DZ_1/Task2.java
@@ -1,5 +1,56 @@
 package DZ_1;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/* Вывести все простые числа от 1 до 1000 */
 public class Task2 {
-    
+
+    public static void main(String[] args) {
+
+        int[] array = createArray(1, 1000);
+        
+        System.out.println(findPrimeNumbers(array).toString());
+
+    }
+
+    private static int[] createArray(int index, int diop) {
+
+        int[] arr = new int [diop+1];
+
+        for(int i = index; i!= arr.length; i++)
+        {
+            arr[i] = i;
+        }
+        return arr;
+    }
+
+    private static List<Integer> findPrimeNumbers(int[] arr){
+        
+        List<Integer> lst = new ArrayList<>();
+
+        for (int index = 1; index < arr.length; index++) {
+
+            for (int jindex = 1; jindex < arr.length; jindex++) {
+                
+                if (jindex > index)
+                {
+                    continue;
+                }
+
+                else{
+                    if(index % jindex == 0 && jindex < index)
+                    {
+                        if(jindex == 1 || jindex == index)
+                        {
+                            lst.add(index);
+                        }
+                                            
+                    }
+                }
+                
+            }
+        }     
+        return lst;
+    }
 }
