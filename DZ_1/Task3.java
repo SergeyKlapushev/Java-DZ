12229269a6e7fe5f34689130ff5466d458dba071
Sergei Klapushev <klapischeff@yandex.ru>
2023-03-11 15:04:35 +0300

    Task3 copmlited

---
+++ DZ_1/Task3.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+import java.util.NoSuchElementException;
+
+public class Task3 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        Scanner scan = new Scanner(System.in);
+        
+
+        Writer("Введите первое число: ");
+        int num1 = imputNumber(scan);
+        sb.append(num1);
+        
+        Writer("Выберите операцию (+, -, *, /): ");
+        String oper = choiseOperation(scan);
+        sb.append(oper);
+
+        Writer("Введите второе число: ");
+        int num2 = imputNumber(scan);
+        sb.append(num2);
+
+        sb.append(" = ");
+
+        int res = result(num1, oper, num2);
+
+        sb.append(res);
+
+        scan.close();
+
+        Writer(sb.toString());
+
+    }
+
+    public static String Writer(String str)
+    {
+        System.out.println(str);
+        return str; 
+    }
+
+    public static int imputNumber(Scanner scan)
+    {
+        Integer num = scan.nextInt();
+        return num;
+    } 
+
+    public static String choiseOperation(Scanner scan)
+    {
+        String operation = scan.next();
+        return operation;
+    }
+
+    public static Integer result (int n1, String op, int n2)
+    {
+        int res = 0;
+        switch (op) {
+            case "+": res = n1 + n2; break;
+
+            case "-": res = n1 - n2; break;
+
+            case "*": res = n1 * n2; break;
+            
+            case "/": res = n1 / n2;
+        }
+
+        return res;
+    }
+
+}
