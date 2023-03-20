792568046b7b839e9e3da700b53491d2c719ba28
Sergei Klapushev <klapischeff@yandex.ru>
2023-03-20 21:30:02 +0300

    Task1 complited

--- DZ_2/Task1.java
+++ DZ_2/Task1.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 /*
@@ -8,35 +10,38 @@ import java.util.Scanner;
 */
 
 public class Task1 {
-  public static void main(String[] args) {  
-    String[][] params = getRequest();
-    StringBuilder request = new StringBuilder();
-    request.append("SELECT * FROM students WHERE ");
-    String prefix = "";
-    for (int i = 0; i < params.length; i++) {
-        if (!params[i][1].toLowerCase().equals("null") && !params[i][1].equals(null)) {
-            request.append(prefix);
-            prefix = " AND ";
-            request.append(params[i][0] + "='" +params[i][1] + "'");
+    public static void main(String[] args) {
+        String[][] params = getRequest();
+        StringBuilder request = new StringBuilder();
+        request.append("SELECT * FROM students WHERE ");
+        String prefix = "";
+        for (int i = 0; i < params.length; i++) {
+            if (!params[i][1].toLowerCase().equals("null") && !params[i][1].equals(null)) {
+                request.append(prefix);
+                prefix = " AND ";
+                request.append(params[i][0]);
+                request.append("='");
+                request.append(params[i][1]);
+                request.append("'");
+            }
         }
+        System.out.println(request);
     }
-    System.out.println(request);
-}
 
-public static String[][] getRequest() {
-    Scanner inp = new Scanner(System.in);
-    String inputString = inp.nextLine();
-    inp.close();
-    if (inputString.isEmpty() || inputString.equals(null)) {
-        System.out.println("Ошибка ввода.");
-        System.exit(0);
+    public static String[][] getRequest() {
+        Scanner inp = new Scanner(System.in);
+        String inputString = inp.nextLine();
+        inp.close();
+        if (inputString.isEmpty() || inputString.equals(null)) {
+            System.out.println("Ошибка ввода.");
+            System.exit(0);
+        }
+        inputString = inputString.replaceAll("[{}\"]", "").replaceAll(", ", ",");
+        String[] inputPairs = inputString.split(",");
+        String[][] output = new String[inputPairs.length][];
+        for (int i = 0; i < inputPairs.length; i++) {
+            output[i] = inputPairs[i].split(":");
+        }
+        return output;
     }
-    inputString = inputString.replaceAll("[{}\"]", "").replaceAll(", ", ",");
-    String[] inputPairs = inputString.split(",");
-    String[][] output = new String[inputPairs.length][];
-    for (int i = 0; i < inputPairs.length; i++) {
-        output[i] = inputPairs[i].split(":");
-    }
-    return output;
-  }
 }     
--- DZ_2/Task3.java
+++ DZ_2/Task3.java
@@ -3,12 +3,74 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
+/* Дана json строка (можно сохранить в файл и читать из файла)
+[{"фамилия":"Иванов","оценка":"5","предмет":"Математика"},
+{"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},
+{"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]
+Написать метод(ы), который распарсит json и, используя StringBuilder, создаст строки вида: Студент [фамилия] получил [оценка] по предмету [предмет].
+Пример вывода:
+Студент Иванов получил 5 по предмету Математика.
+Студент Петрова получил 4 по предмету Информатика.
+Студент Краснов получил 5 по предмету Физика. */
 
 public class Task3 {
-    public static void main(String[] args) {
-        StringBuffer sb = new StringBuffer();
-
-        sb.append(null)
+    public static void main(String[] args) throws IOException {
+        String[][][] fullSplitArray = fullSplit(readLineFromFile("students.txt"));
+        StringBuilder outputLine = new StringBuilder();
+        for (int i = 0; i < fullSplitArray.length; i++) {
+            outputLine.setLength(0);
+            outputLine.append(String.format("Студент %s получил %s по предмету %s.", fullSplitArray[i][0][1], fullSplitArray[i][1][1], fullSplitArray[i][2][1]));
+            System.out.println(outputLine);
+        }
     }
+
+    public static String readLineFromFile(String fileName) throws IOException {
+        try {
+            String pathProject = System.getProperty("user.dir");
+            String pathFile = pathProject.concat("\\" + fileName);
+            String dataLoc = pathFile;
+            BufferedReader bRead = new BufferedReader(new FileReader(dataLoc, Charset.forName("UTF-8")));
+            String inputString = bRead.readLine();
+            bRead.close();
+            return inputString;
+        } catch (Exception e) {
+            throw new IOException(e.getMessage());
+        }
+    }
+
+    public static String[][][] fullSplit(String inputString) {
+        String[] firstSplit = splitPackages(inputString);
+        String[][][] fullSplitArray = new String[firstSplit.length][][];
+        for (int i = 0; i < fullSplitArray.length; i++) {
+            fullSplitArray[i] = splitIntoArrayOfPairs(firstSplit[i]);
+        }
+        return fullSplitArray;
+    }
+
+    public static String[] splitPackages(String inputString) {
+        if (inputString.isEmpty() || inputString.equals(null)) {
+            System.out.println("Ошибка входящих данных.");
+            System.exit(0);
+        }
+        String[] split = inputString.split("\\},\\{");
+
+        for (int i = 0; i < split.length; i++) {
+            split[i] = split[i].replaceAll("[\\[\\]{}\"]", "").replaceAll(", ", ",");
+        }
+        return split;
+    }
+
+    public static String[][] splitIntoArrayOfPairs(String inputString) {
+        String[] inputPairs = inputString.split(",");
+        String[][] output = new String[inputPairs.length][];
+        for (int i = 0; i < inputPairs.length; i++) {
+            output[i] = inputPairs[i].split(":");
+        }
+        return output;
+    }
+        
 }
+
+
