48d03f0f51fc220d0ed2314c4fa299841f8e1a9d
Sergei Klapushev <klapischeff@yandex.ru>
2023-03-23 19:23:23 +0300

    Task1 added

--- DZ_4/Task1.java
+++ DZ_4/Task1.java
@@ -1,14 +1,15 @@
-package DZ_4;
+
+import java.util.LinkedList;
 
 public class Task1 {
+
     public static void main(String[] args) {
-        
-        LinkedList<Integer> linkList = new LinkedList();
+        LinkedList<Integer> linkList = new LinkedList<>();
         linkList.add(1);
         linkList.add(2);
         linkList.add(3);
         linkList.add(4);
         linkList.add(5);
-        System.out.println(linkList.toString());
+
     }
 }
