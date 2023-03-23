f1ca4ac5e8b4343930095dd7c6f08696af5eae50
Sergei Klapushev <klapischeff@yandex.ru>
2023-03-23 19:42:26 +0300

    Task1 complited

--- DZ_4/Task1.java
+++ DZ_4/Task1.java
@@ -1,8 +1,24 @@
 package DZ_4;
 
+import java.util.Collections;
+import java.util.LinkedList;
+
 public class Task1 {
+
     public static void main(String[] args) {
-        
+
+        LinkedList<Integer> linkList = new LinkedList<>();
+        linkList.add(1);
+        linkList.add(2);
+        linkList.add(3);
+        linkList.add(4);
+        linkList.add(5);
+        System.out.println("До - "+ linkList);
+        System.out.println("После - "+ rev(linkList));
     }
-    
+
+    public static LinkedList<Integer> rev (LinkedList<Integer> linkList){
+        Collections.reverse(linkList);
+        return linkList;
+    }
 }
