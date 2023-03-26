b27fda48fceae4d8d6e7f7d1a3a5ef244ac3aec3
Sergei Klapushev <klapischeff@yandex.ru>
2023-03-26 18:13:37 +0300

    Task2

---
+++ DZ_5/Task2.java
@@ -0,0 +1,11 @@
+package DZ_5;
+
+import java.util.HashMap;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        HashMap<String, String> listName = new HashMap<>();
+        listName.put("Иван", "Иванов");
+    }
+}
